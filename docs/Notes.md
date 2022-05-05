ccfec179cd85a101fb3a114145fd75ff367c4c20
Paul Harvey <pharvey@uk.imshealth.com>
2022-05-05 12:18:36 +0100

    Improved build process

--- docs/Notes.md
+++ docs/Notes.md
@@ -3,17 +3,21 @@
 Microinstructions are 48 bits in length with the following format:
 
 ```
-0xSSSSSSAAAA
+0xSSSAA
 ```
 
 Where `S` bytes are control signals, and `A` bytes are the target address for a microcode `jmp` or `jsr` operations.
 
-Microinstructions are converted to little-endian order when written to the output ROM file.
+Microinstructions are converted to little-endian order when written to the output ROM file by `microasm`.
 
 # TO DO
 
 * Remove hardcoded length dependencies from microasm.  *Done but needs testing*
 * Reorder control lines into more logical groupings
+* Add HALT switch
+* Add Reset switch - and initialize via vector
+* Consider using consistent addressing mode bit patterns together with phased uCode execution to automatically have correct addressing code executed and avoid the use subroutines.   This would reduce both execution cycles and code complexity.
+* Sort out an machine code assembler to more easily create test code (dasm?)
 * Consider moving memory decoding logic into a sub-circuit
 * Split PC out to hi and lo and modify signal lines appropriately
 * Add jump operations
@@ -22,3 +26,10 @@ Microinstructions are converted to little-endian order when written to the outpu
 * Investigate using multiple phases of clocks to reduce execution cycles
 * Consider generating HTML as asm output optionally then we can have mouse overs to ID flags and to link to definitions
 
+# References
+
+* 6502 Instruction set - good analysis of bit patterns for addressing modes:
+https://link.springer.com/content/pdf/bbm%3A978-1-349-07360-3%2F1.pdf
+
+* Nice indexed list of opcodes:
+http://www.6502.org/tutorials/6502opcodes.html
--- microasm/Program.cs
+++ microasm/Program.cs
@@ -1,50 +1,50 @@
 ﻿namespace microasm
 {
-    public class MicroAsmRunner
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("MicroAsm");
-            Console.WriteLine("--------");
+   public class MicroAsmRunner
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine("MicroAsm");
+         Console.WriteLine("--------");
+         Console.WriteLine();
+
+         if (args.Length != 2)
+         {
+            Console.WriteLine("Usage: microasm source-file rom-file");
+            return;
+         }
+
+         var inputFile = args[0];
+         var outputFile = args[1];
+
+         Console.WriteLine($"Input file: '{Path.GetFullPath(inputFile)}'");
+         Console.WriteLine($"Output file: '{Path.GetFullPath(outputFile)}'");
+         Console.WriteLine();
+
+         try
+         {
+            var ma = new MicroAsm(inputFile);
+
+            ma.DumpFlagSymbols();
             Console.WriteLine();
 
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Usage: microasm source-file rom-file");
-                return;
-            }
-
-            var inputFile = args[0];
-            var outputFile = args[1];
-
-            Console.WriteLine($"Input file: '{Path.GetFullPath(inputFile)}'");
-            Console.WriteLine($"Output file: '{Path.GetFullPath(outputFile)}'");
+            ma.DumpUCopsSymbols();
             Console.WriteLine();
 
-            try
-            {
-                var ma = new MicroAsm(inputFile);
+            ma.DumpLabelSymbols();
+            Console.WriteLine();
 
-                ma.DumpFlagSymbols();
-                Console.WriteLine();
+            ma.DumpOpsAddrs();
+            Console.WriteLine();
 
-                ma.DumpUCopsSymbols();
-                Console.WriteLine();
+            ma.DumpOutputLog();
 
-                ma.DumpLabelSymbols();
-                Console.WriteLine();
-
-                ma.DumpOpsAddrs();
-                Console.WriteLine();
-
-                ma.DumpOutputLog();
-
-                ma.WriteROMFile(outputFile);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-    }
+            ma.WriteROMFile(outputFile);
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+      }
+   }
 }
