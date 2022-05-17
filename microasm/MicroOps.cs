6c4716a2fe60bd7db62b3ee9e44786c9def3c349
Paul Harvey <pharvey@uk.imshealth.com>
2022-05-17 09:10:58 +0100

    Added two pass parsing to uOps

--- microasm/MicroOps.cs
+++ microasm/MicroOps.cs
@@ -1,99 +1,119 @@
 
 namespace microasm
 {
-    using static MicroAsmConstants;
-    using static MicroAsmFormatting;
+   using System.Linq;
+   using static MicroAsmConstants;
+   using static MicroAsmFormatting;
 
-    public class MicroOps
-    {
-        private readonly Dictionary<string, byte[]> _ucopsSymbols = new();
+   public class MicroOps
+   {
+      private readonly Dictionary<string, byte[]> _resolvedSymbols = new();
+      private readonly HashSet<string> _seenSymbols = new();
 
-        private readonly MicroCtrl _uCtrl;
+      private readonly MicroCtrl _uCtrl;
 
-        private readonly string _sourceFile;
+      private readonly string _sourceFile;
 
-        public MicroOps(MicroCtrl uCtrl, string sourceFile)
-        {
-            _uCtrl = uCtrl;
-            _sourceFile = sourceFile;
-            Parse();
-        }
+      public MicroOps(MicroCtrl uCtrl, string sourceFile)
+      {
+         _uCtrl = uCtrl;
+         _sourceFile = sourceFile;
+         Parse();
+      }
 
-        public bool TryGetValue(string symbol, out byte[] result)
-        {
-            return _ucopsSymbols.TryGetValue(symbol, out result);
-        }
+      public bool TryGetValue(string symbol, out byte[] result)
+      {
+         return _resolvedSymbols.TryGetValue(symbol, out result);
+      }
 
-        private static void Or(byte[] source, byte[] dest)
-        {
-            for (var i = 0; i < source.Length; i++)
-            {
-                dest[i] |= source[i];
-            }
-        }
+      private static void Or(byte[] source, byte[] dest)
+      {
+         for (var i = 0; i < source.Length; i++)
+         {
+            dest[i] |= source[i];
+         }
+      }
 
-        private void ParseUCOpsLine(string line, int lineNumber)
-        {
-            var ucopsParts = line.Split(new char[] { ' ', '\t', '|' },
-                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+      private void ParseUCOpsLine(string line, int lineNumber, int pass)
+      {
+         var ucopsParts = line.Split(new char[] { ' ', '\t', '|' },
+             StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-            if (ucopsParts.Length < 2)
-            {
-                throw new MicroAsmException(
-                    $"Each entry must contain a symbol followed by one or more values",
-                    line, lineNumber, _sourceFile
-                );
-            }
+         if (ucopsParts.Length < 2)
+         {
+            throw new MicroAsmException(
+                $"Each entry must contain a symbol followed by one or more values",
+                line, lineNumber, _sourceFile);
+         }
 
-            var symbol = ucopsParts[0];
-            var value = new byte[WORD_SIZE_IN_BYTES];
+         var symbol = ucopsParts[0];
+         var value = new byte[WORD_SIZE_IN_BYTES];
 
+         // Have we seen the symbol already on the first pass?
+         if (pass == 0 && _seenSymbols.Contains(symbol))
+         {
+            // Must be a duplicate
+            throw new MicroAsmException($"Duplicate symbol '{symbol}'", line, lineNumber, _sourceFile);
+         }
+         _seenSymbols.Add(symbol);
+
+         // Is the symbol already resolved?
+         if (!_resolvedSymbols.ContainsKey(symbol))
+         {
+            bool resolved = true;
+            string currentRhs = null;
             for (var rhsIndex = 1; rhsIndex < ucopsParts.Length; rhsIndex++)
             {
-                var currentRhs = ucopsParts[rhsIndex];
+               currentRhs = ucopsParts[rhsIndex];
 
-                byte[] resolvedSymbol;
-                if (_uCtrl.TryGetValue(currentRhs, out resolvedSymbol))
-                {
-                    Or(resolvedSymbol, value);
-                }
-                else
-                {
-                    throw new MicroAsmException($"Symbol not found '{currentRhs}'", line, lineNumber, _sourceFile);
-                }
+               byte[] resolvedSymbol;
+               if (TryGetValue(currentRhs, out resolvedSymbol) ||
+                   _uCtrl.TryGetValue(currentRhs, out resolvedSymbol))
+               {
+                  Or(resolvedSymbol, value);
+               }
+               else
+               {
+                  resolved = false;
+                  break;
+
+               }
             }
 
-            try
+            if (resolved)
             {
-                _ucopsSymbols.Add(symbol, value);
+               _resolvedSymbols.Add(symbol, value);
             }
-            catch (ArgumentException)
+            else if (pass == 1)
             {
-                throw new MicroAsmException($"Duplicate symbol '{symbol}'",
-                    line, lineNumber, _sourceFile);
+               // Second pass and still not resolved
+               throw new MicroAsmException($"Symbol not found '{currentRhs}'", line, lineNumber, _sourceFile);
             }
-        }
+         }
+      }
 
-        private void Parse()
-        {
+      private void Parse()
+      {
+         for (int pass = 0; pass < 2; pass++)
+         {
             int lineNumber = 1;
-
             foreach (var line in File.ReadLines(_sourceFile))
             {
-                var trimmedLine = line.Trim();
+               var trimmedLine = line.Trim();
 
-                // Skip empty lines and comment lies
-                if (trimmedLine.Length > 0 && !trimmedLine.StartsWith(COMMENT_CHARACTERS))
-                {
-                    ParseUCOpsLine(trimmedLine, lineNumber);
-                }
-                lineNumber++;
+               // Skip empty lines and comment lies
+               if (trimmedLine.Length > 0 && !trimmedLine.StartsWith(COMMENT_CHARACTERS))
+               {
+                  ParseUCOpsLine(trimmedLine, lineNumber, pass);
+               }
+               lineNumber++;
             }
-        }
+         }
+      }
 
-        public override string ToString()
-        {
-            return DumpSymbols(_ucopsSymbols);
-        }
-    }
+      public override string ToString()
+      {
+         return DumpSymbols(_resolvedSymbols);
+      }
+   }
 }
