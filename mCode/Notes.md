34e6db0249b5b71db86c109bec837edab60a8fbc
Paul Harvey <pharvey@uk.imshealth.com>
2022-05-25 12:11:22 +0100

    Added second POST display

---
+++ mCode/Notes.md
@@ -0,0 +1,4 @@
+Many of the examples here, used as test code are taken from http://6502.org/source/.
+
+They have been modified to be compatible with `dasm` and the memory model of the circuit under development.
+
--- mCode/square.asm
+++ mCode/square.asm
@@ -12,72 +12,97 @@
 ;
 ; Destroys all registers
 
-            PROCESSOR 6502
+   PROCESSOR 6502
 
 ; I/O Addresses
 POST = $4001
 
 ; Skip zero page
-            SEG zero-page
-            ORG $0
-            DS $100 
+   SEG   zero-page
+   ORG   $0
+   DS    $100 
 
 ; Skip stack
-            SEG stack
-            ORG $100
-            DS $100
+   SEG   stack
+   ORG   $100
+   DS    $100
 
 ; Code
-            SEG entry
-            ORG $200
+   SEG   entry
+   ORG   $200
 
-            LDA   #$7
-            STA   Numberl
-            LDA   #$0
-            STA   Numberh
-            JSR   Square
-            LDA   Squarel
-            STA   POST
-loop:       JMP   loop
+   LDA   #01
+   STA   POST
+   LDA   #$7
+   STA   Numberl
+   LDA   #$0
+   STA   Numberh
+   JSR   Square
+   LDA   Squarel
+   CMP   #$31
+   BNE   err
+   LDA   Squareh
+   CMP   #$00
+   BNE   err   
+
+
+   LDA   #02
+   STA   POST
+   LDA   #$50
+   STA   Numberl
+   LDA   #$0
+   STA   Numberh
+   JSR   Square
+   LDA   Squarel
+   CMP   #$00
+   BNE   err
+   LDA   Squareh
+   CMP   #$19
+   BNE   err 
+     
+   LDA   #$FF
+   STA   POST
+
+err:
+   JMP   err;   
 
 Square:
-        LDA     #$00        ; clear A
-        STA     Squarel     ; clear square low byte
-                            ; (no need to clear the high byte, it gets
-        LDA	Numberl     ; get number low byte
-	     LDX	Numberh     ; get number high  byte
-	     BPL NoNneg      ; if +ve don't negate it
-                            ; else do a two's complement
-	EOR	#$FF        ; invert
-        SEC	            ; +1
-	ADC	#$00        ; and add it
+   LDA   #$00        ; clear A
+   STA   Squarel     ; clear square low byte
+                     ; (no need to clear the high byte, it gets
+   LDA	Numberl     ; get number low byte
+	LDX	Numberh     ; get number high  byte
+	BPL   NoNneg      ; if +ve don't negate it
+                     ; else do a two's complement
+   EOR   #$FF        ; invert
+   SEC	            ; +1
+   ADC   #$00        ; and add it
 
 NoNneg:
-	STA	Tempsq      ; save ABS(number)
-	LDX	#$08        ; set bit count
+	STA   Tempsq      ; save ABS(number)
+	LDX   #$08        ; set bit count
 
 Nextr2bit:
-	ASL	Squarel     ; low byte *2
-	ROL	Squareh     ; high byte *2+carry from low
-	ASL	           ; shift number byte
-	BCC	NoSqadd     ; don't do add if C = 0
-	TAY                 ; save A
-	CLC                 ; clear carry for add
-	LDA	Tempsq      ; get number
-	ADC	Squarel     ; add number^2 low byte
-	STA	Squarel     ; save number^2 low byte
-	LDA	#$00        ; clear A
-	ADC	Squareh     ; add number^2 high byte
-	STA	Squareh     ; save number^2 high byte
-	TYA                 ; get A back
+	ASL   Squarel     ; low byte *2
+	ROL   Squareh     ; high byte *2+carry from low
+	ASL	            ; shift number byte
+	BCC   NoSqadd     ; don't do add if C = 0
+	TAY               ; save A
+	CLC               ; clear carry for add
+	LDA   Tempsq      ; get number
+	ADC   Squarel     ; add number^2 low byte
+	STA   Squarel     ; save number^2 low byte
+	LDA   #$00        ; clear A
+	ADC   Squareh     ; add number^2 high byte
+	STA   Squareh     ; save number^2 high byte
+	TYA               ; get A back
 
 NoSqadd:
-	DEX                 ; decrement bit count
-	BNE	Nextr2bit   ; go do next bit
+	DEX               ; decrement bit count
+	BNE   Nextr2bit   ; go do next bit
 	RTS
 
-Tempsq:     DC.B    ; temp byte for intermediate result
-            DC.B
+Tempsq:     DC.B     ; temp byte for intermediate result
 Numberl:    DC.B     ; number to square low byte
 Numberh:    DC.B 	   ; number to square high byte
 
