7fb270f2b8bdaa43ab6f0efae287abab7406db56
Paul Harvey <pharvey@uk.imshealth.com>
2022-09-07 16:31:10 +0100

    Adding io

--- simplified/examples/simple.asm
+++ simplified/examples/simple.asm
@@ -9,5 +9,13 @@ main:       LDA #$50
             TAX
             LDA #00
 
+sayhello:   LDX #0
+.next:      LDA hello,X
+            BEQ .done
+            STA $F2
+            INX 
+            JMP .next
+.done:      
 
-         
+hello:      DC    "Hello World" 
+            DC.B  0
