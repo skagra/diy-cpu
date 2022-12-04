e0cc8dc73cbe4b0fa6d0369794b3b486488957a6
Paul Harvey <Paul Harvey>
2022-12-04 12:30:27 +0000

    Updated uCode and test mCode

---
+++ simplified/examples/test.asm
@@ -0,0 +1,49 @@
+            PROCESSOR 6502
+
+; Code
+            SEG entry
+            ORG $00
+
+; Main routine
+main:       
+
+; Register loading
+            LDA #$50
+            LDX #$22
+            LDA #$00
+            LDY #$00
+
+; Register transfer
+            LDA #$67
+            TAX
+            LDX #$56
+            TAX
+
+; Arithmetic
+            LDA #$27
+            ADC #$23 ; Result $4A
+            LDA #$69
+            SBC #$33 ; Result $36
+
+; Looping BEQ
+            LDX #$05
+.nextbeq:   DEX
+            BEQ .beqdone
+            JMP .nextbeq
+.beqdone
+
+; Looping BNE
+            LDX #$05
+.nextbne:   DEX
+            BNE .nextbne
+
+; Indexed addressing
+            LDX #$00
+.nextfib:   LDA .fib,X
+            BEQ .fibdone
+            INX 
+            JMP .nextfib
+.fibdone:   DC.B    0
+
+.fib:       DC.B    1, 1, 2, 3, 5, 8, 0  
+
---
+++ simplified/implemented.md
@@ -0,0 +1,40 @@
+# Implemented OpCodes
+
+## Done
+
+* `ADC`
+    add with carry
+* `BEQ`
+    branch on equal (zero set)
+* `BNE`
+    branch on not equal (zero clear)
+* `DEX`
+    decrement X
+* `INX`
+    increment X
+* `JMP`
+    jump
+* `LDA`
+    load accumulator
+* `LDX`
+    load X
+* `SBC`
+    subtract with carry
+* `TAX`
+    transfer accumulator to X
+* `TXA`
+    transfer X to accumulator
+
+## Implement Once RAM is Added
+
+* `INC`
+    increment
+* `STA`
+    store accumulator
+* `STX`
+    store X
+* `STY`
+    store Y
+* `DEC`
+    decrement
+    
